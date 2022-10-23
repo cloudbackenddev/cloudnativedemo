6bb560e5ebc9ecd7fcd6e3e62f2d143c97d53df0
cloudbackenddev <cloud.backenddev@gmail.com>
2022-10-23 11:38:32 +0530

    first commit

---
+++ src/main/java/com/fictious/app/cloudnativedemo/CloudnativedemoApplication.java
@@ -0,0 +1,13 @@
+package com.fictious.app.cloudnativedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnativedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudnativedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fictious/app/cloudnativedemo/AppControllerTest.java
@@ -0,0 +1,26 @@
+package com.fictious.app.cloudnativedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class AppControllerTest {
+    @Autowired
+    AppController appController;
+
+    @Test
+    void nationsLength() {
+        Integer nationsLength = appController.getRandomNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = appController.getRandomCurrencies().size();
+        assertEquals(20, currenciesLength);
+    }
+
+}
